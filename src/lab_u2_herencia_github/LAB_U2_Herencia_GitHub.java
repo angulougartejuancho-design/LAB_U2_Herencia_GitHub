a3bfafe20c2f78f7928d4a98e419ce4709e90f81
angulougartejuancho <angulougartejuancho@gmail.com>
2026-05-21 13:13:30 -0600

    conexion

---
+++ src/lab_u2_herencia_github/LAB_U2_Herencia_GitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_u2_herencia_github;
+
+/**
+ *
+ * @author angul
+ */
+public class LAB_U2_Herencia_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
